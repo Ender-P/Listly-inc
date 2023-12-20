272033230a672df6c4096803295f10e1ca670b6c
Ender-P <102452037+Ender-P@users.noreply.github.com>
2023-12-20 13:59:22 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ if (history.scrollRestoration) {
         window.scrollTo(0, 0);
     }
 }
+
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('submission');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Success!");
+        document.getElementById('name').value = "";
+        document.getElementById('email').value = "";
+      })
+    });
+  });
+  
+
